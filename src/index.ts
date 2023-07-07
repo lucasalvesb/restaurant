ba58344e322edb4fb43da70a4d051d8f56b45051
Lucas Alves <lucasalves94@gmail.com>
2023-07-06 22:00:30 -0300

    models created

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+
+    app.listen(port, () => {
+      console.log(`Server started on http://localhost:${port}`);
+    });
+
+  })
+  .catch(() => console.log('error connecting to mongo'));
+
